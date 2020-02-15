e742d70fd0ba36e334a06449a7925b3c56eb5f64
Mike-U5 <maikelhaarmans2@gmail.com>
2020-02-15 10:16:50 +0100

    Made Pendant equipable

--- src/main/java/com/shinoow/abyssalcraft/common/items/armor/ItemBaubleTrapezohedron.java
+++ src/main/java/com/shinoow/abyssalcraft/common/items/armor/ItemBaubleTrapezohedron.java
@@ -7,14 +7,17 @@ package com.shinoow.abyssalcraft.common.items.armor;
 import java.util.List;
 
 import baubles.api.BaubleType;
+import baubles.api.BaublesApi;
 import baubles.api.IBauble;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
 import thaumcraft.api.IRunicArmor;
 
 public class ItemBaubleTrapezohedron extends Item implements IBauble, IRunicArmor {
@@ -33,10 +36,23 @@ public class ItemBaubleTrapezohedron extends Item implements IBauble, IRunicArmo
 		return 5;
 	}
 
-	@SuppressWarnings("rawtypes")
-	@SideOnly(Side.CLIENT)
-	public void addInformation(final ItemStack item, final EntityPlayer player, final List list, final boolean bool) {
-		super.addInformation(item, player, list, bool);
+	// Rightclick to equip
+	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+		if(canEquip(stack, player)) {
+			final IInventory baubles = BaublesApi.getBaubles(player);
+			for(int i = 0; i < baubles.getSizeInventory(); i++) {
+				if(baubles.isItemValidForSlot(i, stack)) {
+					final ItemStack stackInSlot = baubles.getStackInSlot(i);
+					if(stackInSlot == null && !world.isRemote) {
+						baubles.setInventorySlotContents(i, stack.copy());
+						player.inventory.setInventorySlotContents(player.inventory.currentItem, null);
+						break;
+					}
+				}
+			}
+		}
+
+		return stack;
 	}
 
 	@Override
